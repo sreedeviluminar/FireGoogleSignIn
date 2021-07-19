d3285dd740fdcf22c83716fb8093e1f4f607167d
sreedevimanu <sreedevimanu2010@gmail.com>
2021-07-19 15:55:47 +0530

    Initial commit

---
+++ app/src/main/java/com/sree/firegooglesignin/MainActivity.java
@@ -0,0 +1,40 @@
+package com.sree.firegooglesignin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+    }
+}
